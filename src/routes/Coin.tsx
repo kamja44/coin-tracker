0ac3b9047a8368af874b0d44340bc12312bdc220
kamja44 <88872409+kamja44@users.noreply.github.com>
2024-06-28 09:39:56 +0900

    Nested Router

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,9 @@
-import { useParams, useLocation } from "react-router";
+import { useParams, useLocation, useMatch } from "react-router";
 import { Container, Header, Loader, Title } from "../components/component";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import { Link, Outlet } from "react-router-dom";
 
 interface IInfo {
   id: string;
@@ -56,31 +59,115 @@ interface IPrice {
     };
   };
 }
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
 
+const Tab = styled.span<{ $isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 20px;
+  font-weight: 700;
+  background-color: rgba(0, 0, 0);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.$isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`;
 const API_URL = "https://api.coinpaprika.com/v1/coins";
 const TICKER_URL = "https://api.coinpaprika.com/v1/tickers";
 function Coin() {
   const { coinId } = useParams();
   const [loading, setLoading] = useState(true);
   const { state } = useLocation();
-  const [ionfo, setInfo] = useState<IInfo>();
-  const [price, setPrice] = useState<IPrice>();
+  const [info, setInfo] = useState<IInfo>();
+  const [priceInfo, setPrice] = useState<IPrice>();
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coinId/chart");
 
   useEffect(() => {
     (async () => {
       const infoData = await (await fetch(`${API_URL}/${coinId}`)).json();
       const priceData = await (await fetch(`${TICKER_URL}/${coinId}`)).json();
-      console.log(infoData, priceData);
       setInfo(infoData);
       setPrice(priceData);
+      setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab $isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab $isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+          <Outlet />
+        </>
+      )}
     </Container>
   );
 }
